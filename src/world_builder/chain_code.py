cfe6f46a43f424dbb3186e7fa37d66b3fa2b5891
Benjamin Helyer <benjaminkhelyer@gmail.com>
2025-04-26 15:11:27 -0500

    added documentation to generate_uuidv7 function

--- src/world_builder/chain_code.py
+++ src/world_builder/chain_code.py
@@ -1,30 +1,43 @@
 """
 This is a module for generating a 'chain code', which is essentially a UUID.
 """
+
 import os
 import time
 from uuid import UUID
 
 
 def generate_uuidv7() -> UUID:
-    timestamp_ms = int(time.time() * 1000)
+    """
+    Generates a UUID which is similar to UUIDv7. UUIDv7 is a UUID variant that includes a timestamp.
 
-    if timestamp_ms >= (1 << 48):
+    Essentially UUIDv7 is timestamp + random sequence, with a few other added bits.
+    Due to the fact that the unique identifier is prefaced by a timestamp, monotonicity is in some situations garunteed.
+    This can eable quicker INSERT operations on a database (in a typical case, INSERTs would simply be an append if the UUID is used as a primary key).
+    Indexing and partitioning can also be improved if one uses UUIDv7 as a primary key, rather than a completely random UUID.
+
+    Returns:
+        UUID: The generated UUIDv7.
+    """
+    timestamp_ms = int(time.time() * 1000)  # timestamp since Unix epoch
+
+    if timestamp_ms >= (
+        1 << 48
+    ):  # defensive programming in case of a huge timestamp value -- this could happen since we are dealing with fictional universes
         raise ValueError("Timestamp too large for UUIDv7")
 
-    rand_bytes = os.urandom(10)
+    rand_bytes = os.urandom(10)  # 10 random bytes, i.e., 80 random bits
     rand_int = int.from_bytes(rand_bytes, "big")
 
     uuid_int = timestamp_ms << 80
-    uuid_int |= 0x7 << 76
-    uuid_int |= 0x2 << 62
+    uuid_int |= 0x7 << 76  # set version bit to 7
+    uuid_int |= 0x2 << 62  # set variant bit to 2
     uuid_int |= rand_int & ((1 << 62) - 1)
 
     return UUID(int=uuid_int)
 
 
-def generate_chain_code(species: str = "unknown", 
-                        is_female:bool =False) -> str:
+def generate_chain_code(species: str = "unknown", is_female: bool = False) -> str:
     """
     Generates the chain code for a character.
     Args:
