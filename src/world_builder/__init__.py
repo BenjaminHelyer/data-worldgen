36dab690a5ab6fe8dc87c506cc77da29f2b577f4
Benjamin Helyer <benjaminkhelyer@gmail.com>
2025-05-02 16:06:17 -0500

    removed old visualizer module which wasn't used from src folder

--- src/world_builder/__init__.py
+++ src/world_builder/__init__.py
@@ -1,4 +1,3 @@
 from .population_config import load_config
 from .chain_code import generate_chain_code
 from .character import create_character, Character
-from .visualizer import create_population_dashboard
