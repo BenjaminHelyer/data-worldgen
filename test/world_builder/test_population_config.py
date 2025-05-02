2327896e3d291b4a7462585b4be35456f6099b77
Benjamin Helyer <benjaminkhelyer@gmail.com>
2025-05-02 17:02:18 -0500

    added medium config file to tests

--- test/world_builder/test_population_config.py
+++ test/world_builder/test_population_config.py
@@ -16,6 +16,7 @@ CONFIG_FILE = CONFIG_DIR / "wb_config_micro.json"
     [
         "wb_config_micro.json",
         "wb_config_small.json",
+        "wb_config_medium.json",
     ],  # known good configs -- should pass
 )
 def test_load_config_smoke(filename):
